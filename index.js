4b0665d1a8653be981c5965a8af018c92cdd9913
Andre Anaya <andre.anaya@deleteagency.com>
2015-12-07 17:51:45 +0000

    remove process module

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-var process = require('process');
 var path = require('path');
 var server = require('./lib/superfluid');
 var isGlobal = require ("is-global");
@@ -9,13 +8,3 @@ if(isGlobal()) {
 }
 
 module.exports = server.init;
-
-
-/*process.on('SIGINT', function() {
-  console.log('STOP mongod');
-});*/
-
-
-/*process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
-});*/
